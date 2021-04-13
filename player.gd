8d8a4de6a97ce9c20b77a1c21ae2b14a282956aa
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2021-04-12 23:53:35 -0400

    add jump to player

--- player.gd
+++ player.gd
@@ -1,6 +1,14 @@
 extends KinematicBody2D
 class_name Player
 
+var _jump_velocity: float = -4
+var _velocity_y: float = 0
+
 func _physics_process(delta):
+	_velocity_y += GameService.gravity * delta
+
 	if (Input.is_action_just_pressed('ui_accept')):
 		$AnimationPlayer.play('fly')
+		_velocity_y = _jump_velocity
+
+	position.y += _velocity_y
